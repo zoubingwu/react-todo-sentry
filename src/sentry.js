d61554506685b8fc0b6a1102fe36fdedec6573df
zoubingwu <zoubingwu@gmail.com>
2021-04-13 15:42:45 +0800

    add sentry in prod only

--- src/sentry.js
+++ src/sentry.js
@@ -2,18 +2,21 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import { history } from "./history";
 
-Sentry.init({
-  dsn: "https://3b736208b89d440b9e86efbd27a6c1e3@o569408.ingest.sentry.io/5715114",
-  integrations: [
-    new Integrations.BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-    }),
-  ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-  release: `${process.env.REACT_APP_NAME}@${process.env.REACT_APP_VERSION}+${process.env.REACT_APP_COMMIT_HASH}`,
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: "https://3b736208b89d440b9e86efbd27a6c1e3@o569408.ingest.sentry.io/5715114",
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+      }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV,
+    release: `${process.env.REACT_APP_NAME}@${process.env.REACT_APP_VERSION}+${process.env.REACT_APP_COMMIT_HASH}`,
+  });
+}
